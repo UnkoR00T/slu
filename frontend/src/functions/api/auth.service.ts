787ee179079dec00612647af63ce131ec122edaa
UnkoR00T <99119433+AlexYTGamePL@users.noreply.github.com>
2025-05-25 12:04:08 +0200

    v0.3#14 Temporally disabled OAuth; till end of beta tests. Added data clearing. Fixed PDF gen path. Added BETA message on first entrance. Added version modal. Added version service, and seen version controller. Added version model message for showing changelog.

--- frontend/src/functions/api/auth.service.ts
+++ frontend/src/functions/api/auth.service.ts
@@ -1,34 +1,44 @@
 import axios from 'axios'
-import {useRoute, useRouter} from 'vue-router'
+import {extractTag} from "@/functions/api/version.service.ts";
+
 
 const api = {
   verify: (token: string | null = localStorage.getItem('jwt')): Promise<void> => {
     return new Promise(async (resolve, reject) => {
-      if(import.meta.env.DEV) resolve();
-      const tempToken = localStorage.getItem('temp_jwt');
-      if (!token) {
-        localStorage.removeItem('jwt');
-        window.location.href = `${import.meta.env.VITE_MAIN_SSO}site?app=Unihokej`;
-        reject();
+      const been = localStorage.getItem("been");
+      if(!been){
+        axios.get("https://api.github.com/repos/UnkoR00T/slu/commits").then(res => {
+          alert(`Ver: ${extractTag(res.data[0].commit.message)} \nThis is a test version! There may be bugs. Please report any problems or inconveniences to the site creator.`);
+          localStorage.setItem("been", "true");
+        })
       }
-      await axios.post(`${import.meta.env.VITE_MAIN_BACKEND}verify`, null, {
-        headers: {
-          Authorization: `Bearer ${tempToken}`
-        },
-      }).then(async (res) => {
-        if (res.status == 401) {
-          await assignToken(token).then(() => resolve());
-        } else if(res.status == 200) {
-          if(token){
-            localStorage.setItem('jwt', token);
-          }
-        }
-      }).catch(async (e) => {
-        if(e.status == 401){
-          return await assignToken(token);
-        }
-      })
       resolve();
+
+      // if(import.meta.env.DEV) resolve();
+      // const tempToken = localStorage.getItem('temp_jwt');
+      // if (!token) {
+      //   localStorage.removeItem('jwt');
+      //   window.location.href = `${import.meta.env.VITE_MAIN_SSO}site?app=Unihokej`;
+      //   reject();
+      // }
+      // await axios.post(`${import.meta.env.VITE_MAIN_BACKEND}verify`, null, {
+      //   headers: {
+      //     Authorization: `Bearer ${tempToken}`
+      //   },
+      // }).then(async (res) => {
+      //   if (res.status == 401) {
+      //     await assignToken(token).then(() => resolve());
+      //   } else if(res.status == 200) {
+      //     if(token){
+      //       localStorage.setItem('jwt', token);
+      //     }
+      //   }
+      // }).catch(async (e) => {
+      //   if(e.status == 401){
+      //     return await assignToken(token);
+      //   }
+      // })
+      // resolve();
     })
   }
 }
@@ -54,5 +64,7 @@ const assignToken = async (jwt: string | null): Promise<void> => {
     }
   })
 }
+
+
 
 export default api;
