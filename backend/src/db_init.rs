df4b1d85b8a16eaa5de123c58d3974fd88b96c7e
UnkoR00T <unkor00t@gmail.com>
2025-05-30 13:24:34 +0200

    v0.3#016 Added check if debug mode enabled; if yes disable tracing. Added log tracing for loki and grafana Added db connection and MONGO_URI Added db user struct Added global Uuid type

--- backend/src/db_init.rs
+++ backend/src/db_init.rs
@@ -2,9 +2,7 @@ use std::env;
 use mongodb::{Client, Database};
 
 pub async fn db_init() -> Database {
-    let mongo_uri = env::var("MONGO_URI").unwrap_or_else(|_| {
-        "mongodb://localhost:27017".to_string()
-    });
+    let mongo_uri = env::var("MONGO_URI").unwrap();
 
     let client = Client::with_uri_str(&mongo_uri)
         .await
