d98747da4222d161446ae829714d729ad0885e29
UnkoR00T <unkor00t@gmail.com>
2025-05-30 14:01:12 +0200

    v0.3#017 Added check if debug mode enabled; if yes disable tracing. Added log tracing for loki and grafana Added db connection and MONGO_URI Added db user struct Added global Uuid type

--- backend/src/routes/auth/login.rs
+++ backend/src/routes/auth/login.rs
@@ -41,7 +41,7 @@ pub async fn login(input: Json<TokenInput>, db: &State<Database>) -> Result<Stri
         }
         Err(e) => {
             error!("DBQueryError - Failed to insert data.");
-            info!(e);
+            info!("{}", e);
             Err(Status::InternalServerError)
         }
     }
